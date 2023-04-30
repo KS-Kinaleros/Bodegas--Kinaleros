a30b8485f22362f752c05b971e8a2663b77d51d9
Fmarroquin <fmarroquin-2018482@kinal.edu.gt>
2023-04-30 01:50:07 -0600

    Se actualizo el modelo, contralador y rutas de lease

---
+++ Bodegas-Back-End/src/lease/lease.routes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express');
+const api = express.Router();
+const leaseController = require('./lease.controller');
+
+api.get('/test', leaseController.test)
+api.post('/save', leaseController.saveLease)
+api.put('/update/:id', leaseController.updateLease)
+api.delete('/delete/:id', leaseController.deleteLease)
+api.get('/get', leaseController.getLease)
+
+module.exports = api;
