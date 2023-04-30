3613a62f6c7c1aa9fcfb25f687fad0a42de0c39e
Fmarroquin <fmarroquin-2018482@kinal.edu.gt>
2023-04-29 23:31:52 -0600

    Se agrego el user default al correr el proyecto

--- Bodegas-Back-End/index.js
+++ Bodegas-Back-End/index.js
@@ -3,6 +3,8 @@
 require('dotenv').config()
 const mongoConfig = require('./configs/mongo');
 const app = require('./configs/app');
+const userDef = require('./src/user/user.controller')
 
+userDef.userDefault();
 mongoConfig.connect();
 app.initServer();
