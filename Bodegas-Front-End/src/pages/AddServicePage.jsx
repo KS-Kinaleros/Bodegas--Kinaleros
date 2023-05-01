a4d090e23ee86ecb531a31089b764b3c3912f03a
Fmarroquin <fmarroquin-2018482@kinal.edu.gt>
2023-05-01 17:47:41 -0600

    Se agregaron los gets de todas las entidades

--- Bodegas-Front-End/src/pages/AddServicePage.jsx
+++ Bodegas-Front-End/src/pages/AddServicePage.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CardAddService } from '../components/CardAddService'
+import axios from 'axios'
 
 export const AddServicePage = () => {
+  const [addService, setAddService] = useState([{}])
+
+  const getAddService = async () => {
+    try {
+      const { data } = await axios('http://localhost:3200/service/get')
+      if (data.addService) {
+        setAddService(data.addService)
+        console.log(data.addService)
+      }
+    } catch (err) {
+      console.log(err)
+      throw new Error(err.response.message || 'Error getting Additional Services')
+    }
+  }
+ 
+   useEffect(() => getAddService, []) 
+
   return (
-    <div>AddServicePage</div>
+    <>
+      <main>
+        <div className="left binding color">
+          <i className="fa-solid fa-plus"></i>
+          |  CONTROL ADDITIONAL SERVICES
+        </div>
+
+
+
+        <div className='row g-0 justify-content-center'>
+          {
+            addService.map(({ name, description }, i) => {
+              return (
+                <CardAddService
+                  key={i}
+                  title={name}
+                  description={description}
+                >
+                </CardAddService>
+              )
+            })
+          }
+        </div>
+      </main>
+    </>
   )
 }
--- Bodegas-Front-End/src/pages/LeasePage.jsx
+++ Bodegas-Front-End/src/pages/LeasePage.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { CardLease } from '../components/CardLease'
+import axios from 'axios'
 
 export const LeasePage = () => {
+  const [cellars, setCellars] = useState([{}])
+
+  const getLease = async () => {
+    try {
+      const { data } = await axios('http://localhost:3200/lease/get')
+      if (data.lease) {
+        setCellars(data.lease)
+        console.log(data.lease)
+      }
+    } catch (err) {
+      console.log(err)
+      throw new Error(err.response.message || 'Error getting Lease')
+    }
+  }
+
+  useEffect(() => getLease, [])
+
   return (
-    <div>LeasePage</div>
+    <>
+      <main>
+        <div className="left binding color">
+        <i class="fa-solid fa-warehouse"></i>
+          | CONTROL LEASES
+        </div>
+
+        <div className='row g-0 justify-content-center'>
+          {
+            cellars.map(({ cellar, client, worker, total }, i) => {
+              return (
+                <CardLease
+                  key={i}
+                  title={cellar?.name}
+                  client={client?.name}
+                  worker={worker?.name}
+                  total={total}
+                ></CardLease>
+              )
+            })
+          }
+        </div>
+      </main>
+    </>
   )
 }
+
