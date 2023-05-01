e80e66c3ad8a2e98bf65f6e92cbff0fd779f71fb
Fmarroquin <fmarroquin-2018482@kinal.edu.gt>
2023-05-01 17:47:41 -0600

    Se agregaron los gets de todas las entidades

--- Bodegas-Front-End/src/pages/CellarPage.jsx
+++ Bodegas-Front-End/src/pages/CellarPage.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { CardCellar } from '../components/CardCellar'
 
 export const CellarPage = () => {
+  const [cellars, setCellars] = useState([{}])
+
+  const getCellars = async () => {
+    try {
+      const { data } = await axios('http://localhost:3200/cellar/get')
+      if (data.cellar) {
+        setCellars(data.cellar)
+        console.log(data.cellar)
+      }
+    } catch (err) {
+      console.log(err)
+      throw new Error(err.response.message || 'Error getting Cellars')
+    }
+  }
+
+  useEffect(() => getCellars, [])
+
   return (
-    <div>CellarPage</div>
+    <>
+      <main>
+        <div className="left binding color">
+        <i class="fa-solid fa-warehouse"></i>
+          |  CONTROL CELLAR
+        </div>
+
+        <div className='row g-0 justify-content-center'>
+          {
+            cellars.map(({ name, description, location }, i) => {
+              return (
+                <CardCellar
+                  key={i}
+                  title={name}
+                  description={description}
+                  location={location}
+                ></CardCellar>
+              )
+            })
+          }
+        </div>
+      </main>
+    </>
   )
 }
--- Bodegas-Front-End/src/pages/UserPage.jsx
+++ Bodegas-Front-End/src/pages/UserPage.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { CardUsers } from '../components/CardUsers'
+import axios from 'axios'
 
 export const UserPage = () => {
+  const [users, setUsers] = useState([{}])
+  /*  const headers = {
+     'Content-Type': 'application/json',
+     'Authorization': localStorage.getItem('token')
+   } */
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios('http://localhost:3200/user/getUsers')
+      if (data.users) {
+        setUsers(data.users)
+        console.log(data.users)
+      }
+    } catch (err) {
+      console.log(err)
+      throw new Error(err.response.message || 'Error getting users')
+    }
+  }
+
+
+  useEffect(() => getUsers, [])
+
   return (
-    <div>UserPage</div>
+    <>
+      <main>
+        <div className="left binding color">
+          <i className="fa-regular fa-user"></i>
+          | CONTROL USERS
+        </div>
+        <div className='row g-0 justify-content-center'>
+          {
+            users.map(({ name, surname, phone, email }, i) => {
+              return (
+                <CardUsers
+                  key={i}
+                  title={name}
+                  surname={surname}
+                  phone={phone}
+                  email={email}
+                ></CardUsers>
+              )
+            })
+          }
+        </div>
+      </main>
+    </>
   )
 }
+
